efc08d76c80c0a29bf64351654c24d15344608d8
Seth Hein <me@reallyseth.com>
2015-10-05 14:54:24 -0500

    Fixed auto layout constraint error on edit profile VC
    Also added the class for editing the looking for data.

---
+++ Matchboard/ViewControllers/EditLookingForViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditLookingForViewController.swift
+//  Matchboard
+//
+//  Created by Seth Hein on 10/5/15.
+//  Copyright © 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class EditLookingForViewController: UIViewController {
+
+}

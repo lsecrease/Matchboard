b875ea2fea308d54bd35fa62717e311bba4712e3
Seth Hein <me@reallyseth.com>
2015-10-05 16:07:28 -0500

    Storyboard for Edit Looking For View Controller
    Also removed the 'Block' button when viewing your own profile.

--- Matchboard/ViewControllers/AdProfileViewController.swift
+++ Matchboard/ViewControllers/AdProfileViewController.swift
@@ -32,7 +32,7 @@ enum AdColumns : String {
     case username = "username"
 }
 
-class AdProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, BioCellDelegate, AboutMeCellDelegate, LookingForCellDelegate, BlockUserDelegate, EditAboutMeDelegate, EditProfileDelegate {
+class AdProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, BioCellDelegate, AboutMeCellDelegate, LookingForCellDelegate, BlockUserDelegate, EditAboutMeDelegate, EditProfileDelegate, EditLookingForDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -82,8 +82,7 @@ class AdProfileViewController: UIViewController, UITableViewDataSource, UITableV
                     }
                 }
             }
-        } else if segue.identifier == "EditProfileSegue"
-        {
+        } else if segue.identifier == "EditProfileSegue" {
             if let navVC = segue.destinationViewController as? UINavigationController {
                 if let editProfileVC = navVC.topViewController as? EditProfileViewController
                 {
@@ -108,6 +107,15 @@ class AdProfileViewController: UIViewController, UITableViewDataSource, UITableV
                             editProfileVC.age = age
                         }
                     }
+                }
+            }
+        } else if segue.identifier == "EditLookingForSegue" {
+            if let navVC = segue.destinationViewController as? UINavigationController {
+                if let editProfileVC = navVC.topViewController as? EditLookingForViewController
+                {
+                    editProfileVC.delegate = self
+                    
+                    // TODO: pass data
                 }
             }
         }
@@ -181,7 +189,7 @@ class AdProfileViewController: UIViewController, UITableViewDataSource, UITableV
     // MARK: - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 4
+        return isMine ? 3 : 4
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
@@ -497,6 +505,18 @@ class AdProfileViewController: UIViewController, UITableViewDataSource, UITableV
     
     func profileCancelled(sender: AnyObject)
     {
+        sender.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - EditLookingForDelegate
+    
+    func lookingForSaved(sender: AnyObject, classifiedString: String, lookingForString: String, images: [UIImage]?) {
+        
+        // TODO: save the stuff
+        sender.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func lookingForCancelled(sender: AnyObject) {
         sender.dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- Matchboard/ViewControllers/EditLookingForViewController.swift
+++ Matchboard/ViewControllers/EditLookingForViewController.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+protocol EditLookingForDelegate
+{
+    func lookingForSaved(sender: AnyObject, classifiedString: String, lookingForString: String, images: [UIImage]?)
+    func lookingForCancelled(sender: AnyObject)
+}
+
 class EditLookingForViewController: UIViewController {
 
+    var delegate : EditLookingForDelegate?
+    
+    @IBOutlet weak var lookingForTextView: UITextView!
+    @IBOutlet weak var classifiedField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        delegate?.lookingForCancelled(navigationController!)
+    }
+    @IBAction func saveButtonPressed(sender: AnyObject) {
+        delegate?.lookingForSaved(navigationController!, classifiedString: classifiedField.text!, lookingForString: lookingForTextView.text!, images: [])
+    }
 }
